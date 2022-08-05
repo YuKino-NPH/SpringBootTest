a2e399b0315aad9b1f17f4b7533abc84ead49a8b
YuKino-NPH <82917141+YuKino-NPH@users.noreply.github.com>
2022-08-05 15:55:39 +0800

    Update SpringbootTest1Application.java
    GIT测试 GIT测试 [what] 测试GIT远程分支和本地分支不同时的合并操作

--- main/java/com/cvte/springboottest1/SpringbootTest1Application.java
+++ main/java/com/cvte/springboottest1/SpringbootTest1Application.java
@@ -2,6 +2,9 @@ package com.cvte.springboottest1;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 测试远程分支修改后与本地分支合并
+ */
 @SpringBootApplication
 @MapperScan("com.cvte.springboottest1.mapper")
 public class SpringbootTest1Application {
